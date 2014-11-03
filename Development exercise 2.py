9b7d3966b3077176e2337951ae667c2114aec1e8
ashleynkomo <ankomo@longroad.ac.uk>
2014-11-03 19:14:47 +0000

    hw

---
+++ Development exercise 2.py
@@ -0,0 +1,13 @@
+#Ashley Nkomo
+#03-11-14
+#Development exercise 2
+
+
+stars = int(input("Please enter the number of stars per row: "))
+rows = int(input("Please enter the number of rows to be displayed: "))
+
+numberofstars = stars * "*"
+
+
+for doesntmatterwhatthisis in range(rows):
+    print(numberofstars)
---
+++ Revision exercise 4.py
@@ -0,0 +1,12 @@
+#Ashley Nkomo
+#03-11-14
+#Revision Exercise 4
+
+spread = False
+
+while not spread:
+    number = int(input("Please enter a number in the range 10 to 20:"))
+    if 10 <= number <= 20:
+        spread = True
+        print("This number is within range")
+
